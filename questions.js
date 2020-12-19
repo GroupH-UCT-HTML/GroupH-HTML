178481d9367538e03bfa22336f5ab37bdb64f9dc
GroupH-UCT-HTML <76265862+GroupH-UCT-HTML@users.noreply.github.com>
2020-12-19 12:59:17 -0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,118 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "when a website consist only html and css is know as?",
+    answer: "Static",
+    options: [
+      "Dynamic",
+      "Static",
+      "Responsive",
+      "automatic"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "What does NAN Mean?",
+    answer: "Not a Number",
+    options: [
+      "Not a Number",
+      "No a number",
+      "Next A number",
+      "Not a now"
+    ]
+  },
+
+  
+  {
+    numb: 7,
+    question: "There's 3 types of CSS which one below is not ?",
+    answer: "implement CSS",
+    options: [
+      "Enline CSS",
+      "Empedded CSS",
+      "External CSS",
+      "implement CSS"
+    ]
+  },
+  
+  {
+    numb: 8,
+    question: "What's a funtion?",
+    answer: "A function is a block of code that performs a specific task",
+    options: [
+      "A function is a block of code that performs a specific task",
+      "Funtion is a reserved-word that perfoms action on value and variables",
+      "funtion is branch of arrays",
+      "funtion is like objects"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "What does DOM stand For?",
+    answer: "Document Object Model",
+    options: [
+      "Datatype and methods",
+      "Document object method",
+      "Document Object Model",
+      "Document optimism manner"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What does loops do?",
+    answer: "Loops are used to do something repeatedly",
+    options: [
+      "Loops is a block of code that performs a specific task",
+      "Loops are the creation of variables",
+      "loop is a box which allows the user to enter input by providing a text box",
+      "Loops are used to do something repeatedly"
+    ]
+  },
+
+];
